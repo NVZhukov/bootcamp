bd47d5df750485c159832acb911aac674bc73037
Zhukov Nikita <nypno@mail.ru>
2022-11-09 19:01:31 +0300

    added buble sort folder

---
+++ BubleSort/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+Сортировка пузырьком:
+Время выполнения : O (n ^ 2)
+Память : O (n)
+*/
+
+Console.WriteLine("Введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine("Введите элемент массива: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine("Начальный массив: [" + string.Join(", ", array) + "]");
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n - 1; j++)
+    {
+        if (array[j] > array[j + 1])
+        {
+            int temp = array[j];
+            array[j] = array[j + 1];
+            array[j + 1] = temp;
+        }
+    }
+    Console.WriteLine(i + "[" + string.Join(", ", array) + "]");
+}
+Console.WriteLine("Отсортированный массив: [" + string.Join(", ", array) + "]");
